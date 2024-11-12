1c63f09453b1de7ec670bdb5198c21a8f8bc7abf
JerryHVK <khanghoangvan29042003@gmail.com>
2024-11-12 17:32:01 +0700

    init

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # IoT_Course_Project_2024.1
+
+1. Run `npm install` to get neccessary package define in `package.json`
---
+++ server.js
@@ -0,0 +1,21 @@
+const mongoose = require('mongoose');
+const dotenv = require('dotenv'); // to get the environment variable
+dotenv.config({ path: './config.env' });
+
+const app = require('./app');
+
+mongoose
+  .connect(process.env.DATABASE_LOCAL, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+  })
+  .then(() => {
+    console.log('DB connected successfully!');
+  });
+
+const port = process.env.PORT;
+
+app.listen(port, () => {
+  console.log(`App running on port ${port}...`);
+});
